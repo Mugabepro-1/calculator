0cfd10ac706461307f24911c78a00f9416811140
Mugabepro-1 <mugabepromesse@gmail.com>
2024-03-11 10:15:55 +0200

    It is Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports.addNums = function addNums(num1, num2, num3, num4){
+    return num+num2+num3+num4
+}
+module.exports.subNums = function subNums(num1, num2){
+    return num-num2
+}
+module.exports.multNums = function multNums(num1, num2){
+    return num*num2
+}
